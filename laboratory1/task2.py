2debad668327ec730229d00f2300ec3d09b24682
Lada678 <kondrashova.lada@gmail.com>
2019-10-01 22:58:27 +0300

    Задача 2

---
+++ laboratory1/task2.py
@@ -0,0 +1,15 @@
+print('Лабораторна робота номер 1, варіант 9, задача 2')
+print('Кондрашової В.О. КМ-93')
+print('Знаходження кількості цілих  чисел введених  з клавіатури')
+count = 0
+a = float(input('Введите 1 число: '))
+if int(a) == a:
+    count = count + 1
+b = float(input('Введите 2 число: '))
+if int(b) == b:
+    count = count + 1
+c = float(input('Введите 3 число: '))
+if int(c) == c:
+    count = count + 1
+print('Кількість цілих чисел серед a,b,c:', count)
+
